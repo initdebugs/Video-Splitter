580c5970b66140ce270c141c985c5cc14be7f4f0
Lesley Natrop <75781464+initdebugs@users.noreply.github.com>
2022-01-31 16:38:45 +0100

    Option to select output directory

--- split.py
+++ split.py
@@ -1,20 +1,32 @@
 #Developed by @Initdebugs
+from tkinter import DISABLED
 import PySimpleGUI as sg
 import cv2
 import os.path
 
 file_selection = [
     [
-        sg.T("Select video", key="input"),
-        sg.In(size=(25, 1), enable_events=True, key="-VIDEO-"),
+        sg.T("Select video ", key="input"),
+        sg.In(size=(25, 1), disabled=True , enable_events=True, key="-VIDEO-"),
         sg.FileBrowse(file_types=(("MP4 files", "*.mp4"),)),
         sg.T("Welcome! Select a video to begin...", key="splitStatus"),
+    ]
+]
+output_selection = [
+    [
+        sg.T("Output folder"),
+        sg.In(size=(25, 1), disabled=True , enable_events=True, key="-OUTPUT-"),
+        sg.FolderBrowse(),
+        sg.T("", key="outputStatus"),
     ]
 ]
 
 layout = [
     [
         sg.Column(file_selection),
+    ],
+    [
+        sg.Column(output_selection),
     ],
     [
         sg.Radio('1 frame', "frameRadio", key="frame1", default=True),
@@ -25,6 +37,12 @@ layout = [
         sg.Radio('120 frames', "frameRadio", key="frame120", default=False),           
         sg.Button("Start"),
         sg.Button("Stop"),
+    ],
+    [
+        sg.Radio('Custom', "frameRadio", key="framecustom", default=False),
+        sg.Spin([i for i in range(1,2000)], size=(10, 10),initial_value=1, key="customSpin"),
+        sg.VSep(),
+        sg.T("", key="processingText" ),
     ],
     [
         sg.ProgressBar(1, orientation='h', size=(60, 10), border_width=4, key='progbar',bar_color=['Green','#a6a6a6'])
@@ -40,65 +58,62 @@ while True:
         break
     if event == "Start":
         filename = values["-VIDEO-"]
+        outputDir = values["-OUTPUT-"]
+
         if filename == "":
             window["splitStatus"].update("Please select a video first...")
             continue
-        else:
-            if not filename.endswith(".mp4"):
-                continue
-            window["splitStatus"].update("Starting...")
-    
-            frames = 0
-            if values["frame1"] == True:
-               frames = 1
-            elif values["frame2"] == True:
-               frames = 2
-            elif values["frame5"] == True:
+        if outputDir == "":
+            window["outputStatus"].update("Please select an output folder first...")
+            continue
+        elif not len(os.listdir(outputDir)) == 0:
+            window["outputStatus"].update("The selected folder is not empty")
+            continue
+        window["splitStatus"].update("Starting...")
+        customFrameNumber = values["customSpin"]
+
+        frames = 0
+        if values["frame1"] == True:
+            frames = 1
+        elif values["frame2"] == True:
+            frames = 2
+        elif values["frame5"] == True:
                 frames = 5
-            elif values["frame10"] == True:
-                frames = 10
-            elif values["frame30"] == True:
-                frames = 30
-            elif values["frame120"] == True:
-                frames = 120        
+        elif values["frame10"] == True:
+            frames = 10
+        elif values["frame30"] == True:
+            frames = 30
+        elif values["frame120"] == True:
+            frames = 120   
+        elif values["framecustom"] == True:
+            frames = customFrameNumber     
         
-            progress_bar = window.FindElement('progbar')
+        progress_bar = window.FindElement('progbar')
 
-            count = 0
-            cap = cv2.VideoCapture(filename)
-            frameCount =  int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        count = 0
+        cap = cv2.VideoCapture(filename)
+        frameCount =  int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-            outputDir = ("Frames")
-            dirName = outputDir + str(folderCount)
-            checkDir = os.path.isdir(dirName)
-            
-            while checkDir:
-                dirName = outputDir + str(folderCount)
-                if not checkDir == os.path.isdir(dirName):
-                    checkDir = False
-                    break
-                folderCount+=1
+        while True:
+            window["splitStatus"].update("...")
+            window["outputStatus"].update("")
 
-            # If folder doesn't exist, then create it.
-            if not checkDir:
-                os.makedirs(dirName)
-                print("Created folder: ", outputDir)
+            if event == "Stop":
+                break
+            event, values = window.read(timeout=20)
+            if event == "Exit" or event == sg.WIN_CLOSED:
+                break
+            success,image = cap.read()
+            if success == True:
+                cv2.imwrite(values["-OUTPUT-"]+"/frame%d.jpg" % count, image)     # save frame as JPEG file      
+                print('Read a new frame: ', success)
 
-            while True:
-                window["splitStatus"].update("Splitting video...")
-                if event == "Stop":
-                    break
-                event, values = window.read(timeout=20)
-                if event == "Exit" or event == sg.WIN_CLOSED:
-                    break
-                success,image = cap.read()
-                if success == True:
-                    cv2.imwrite(dirName+"/frame%d.jpg" % count, image)     # save frame as JPEG file      
-                    print('Read a new frame: ', success)
+                count += frames
+                progress_bar.UpdateBar(count,frameCount)
+                window["processingText"].update("Processing... " + str(count) + " of " + str(frameCount) + " frames")
+                cap.set(cv2.CAP_PROP_POS_FRAMES, count)
+            else:
+                window["processingText"].update("Processing done.")
+                window["splitStatus"].update("Ready!")
 
-                    count += frames
-                    progress_bar.UpdateBar(count,frameCount)
-                    cap.set(cv2.CAP_PROP_POS_FRAMES, count)
-                else:
-                    window["splitStatus"].update("Done.")
-                    break
+                break
